a5939050a869148bc62c2b72c0a00f0b4bd21bf9
Celdama <celdama.dev@gmail.com>
2022-01-14 17:40:31 +0100

    GlobalStyle: change bg color

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -17,13 +17,16 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: 'Karla', sans-serif;
-    background-color: palegoldenrod;
+    background-color: #e76f51;
     height: 100vh;
   }
 
   #root {
     padding: 36px 30px;
     background-color: #0B2434;
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+
+    border-radius: 10px;
   }
 
   .App {
@@ -35,6 +38,7 @@ export const GlobalStyle = createGlobalStyle`
     border-radius: 5px;
     width: 410px;
     height: 429px;
+
 
     .title {
       font-size: 40px;
@@ -59,7 +63,7 @@ export const GlobalStyle = createGlobalStyle`
       background-color: #5035FF;
       width: 100px;
       height: 50px;
-      box-shadow: 0px 3.2px 7.68px rgba(0, 0, 0, 0.18);
+      box-shadow: 0px 3.2px 7.5px rgba(0, 0, 0, 0.18);
       border: none;
       margin-top: 30px;
       border-radius: 6px;
